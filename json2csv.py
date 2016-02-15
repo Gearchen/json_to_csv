14c1d02b09267234984762bce90c70a969ba6bd4
Chen Chen <1015613315@qq.com>
2016-02-15 22:05:00 +0800

    update code

--- json2csv.py
+++ json2csv.py
@@ -32,26 +32,23 @@ def transformData(primaryKey=""):
         for j in i.keys():
             if j not in header:
                 header.append(j)
-    with open(outputFileName+".csv", 'wb') as output_file:
-        fieldnames = list(header)
-        writer = csv.DictWriter(output_file, fieldnames, delimiter=',', quotechar='"')
-        result.append(header)
-        for x in data:
-            row_value = {}
-            for y in x.keys():
-                yValue = x.get(y)
-                if type(yValue) == int or type(yValue) == bool or type(yValue) == float or type(yValue) == list:
-                    row_value[y] = str(yValue).encode('utf8')
-                elif type(yValue) == unicode:
-                    row_value[y] = yValue.encode('utf-8')
-            result.append(row_value.values())
-    return fieldnames,result
+    result.append(header)
+    for x in data:
+        row_value = {}
+        for y in x.keys():
+            yValue = x.get(y)
+            if type(yValue) == int or type(yValue) == bool or type(yValue) == float or type(yValue) == list:
+                row_value[y] = str(yValue).encode('utf8')
+            elif type(yValue) == unicode:
+                row_value[y] = yValue.encode('utf-8')
+        result.append(row_value.values())
+    return result
 
 if __name__ == '__main__':
     url = sys.argv[1]
     data_load = GetUrl(url) # load data from the API
     jsonFile(data_load) # write to json file
-    filednames,trans_data = transformData() # transform json to csv
+    trans_data = transformData() # transform json to csv
     with open('jsonToCsvResult.csv','wb') as f:
         writer = csv.writer(f)
         writer.writerows(trans_data)
